1e30f0d5532c5911a251324096e66ae58be363f7
Pablo Fernandez <pablofernandez.fi@gmail.com>
2019-11-05 10:22:11 -0600

    All inserts finished

--- ProjectCode/src/models/DBCreate.java
+++ ProjectCode/src/models/DBCreate.java
@@ -46,6 +46,11 @@ public class DBCreate {
 			insertDummyRecordsStudents();
 			// Inserting professors on the data base
 			insertDummyRecordsProfessors();
+			// Inserting universities on the data base
+			insertDummyRecordsUniversity();
+			// Inserting courses on the data base
+			insertDummyRecordsCourses();
+		
 	}
 	
 	/**
@@ -87,6 +92,51 @@ public class DBCreate {
 		DB.insertProfessor(p3);
 		DB.insertProfessor(p4);
 		DB.insertProfessor(p5);
+		System.out.println("Insertion completed");
+	}
+	
+	/**
+	 * INSERT INTO METHOD for dummy initial records
+	 */
+	public void insertDummyRecordsUniversity() {
+		// Execute a query
+		System.out.println("Inserting dummy records of university into the table...");
+
+		University u1 = new University(100, "IIT", "Illinois Institute of Technology", "Chicago","60616");
+		University u2 = new University(200, "UPM", "Universidad Politecnica de Madrid", "Madrid","28882");
+		
+		DB.insertUniversity(u1);
+		DB.insertUniversity(u2);
+		System.out.println("Insertion completed");
+	}
+	
+		/**
+	 * INSERT INTO METHOD for dummy initial records
+	 */
+	public void insertDummyRecordsCourses() {
+		// Execute a query
+		System.out.println("Inserting dummy records of courses into the table...");
+		
+		Professor p1 = new Professor("Luke", "Papademas", "lpapademas", "ITMD","ITM",342);
+		Professor p2 = new Professor("James", "Papademas", "lpapademas", "ITMD","ITM",163);
+		Professor p3 = new Professor("Dr", "Mo", "dmo", "ITMS","ITM",643);
+		Professor p4 = new Professor("Ramesh", "Rao", "rrao", "ITME","ITM",281);
+		Professor p5 = new Professor("Jeremy", "Hajek", "jhajek", "ITMD","ITM",207);
+		
+		University u1 = new University(100, "IIT", "Illinois Institute of Technology", "Chicago","60616");
+		University u2 = new University(200, "UPM", "Universidad Politecnica de Madrid", "Madrid","28882");
+
+		Course c1 = new Course("Database Security", 101, 3, p3,u1);
+		Course c2 = new Course("Programacion Concurrente", 201, 6, p2,u2);
+		Course c3 = new Course("Security Topics", 102, 3, p3,u1);
+		Course c4 = new Course("Inteligencia Artifical", 202, 3, p5,u2);
+		Course c5 = new Course("Vendor Management", 103, 3, p4,u1);
+		
+		DB.insertCourse(c1);
+		DB.insertCourse(c2);
+		DB.insertCourse(c3);
+		DB.insertCourse(c4);
+		DB.insertCourse(c5);
 		System.out.println("Insertion completed");
 	}
 }
