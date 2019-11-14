5fddc3ef4f4831abf500afca856bba2acaeb87e9
pabloaaf <palvaf02@estudiantes.unileon.es>
2019-11-13 19:21:12 -0600

    profesor show and edit

--- ProjectCode/src/professor/IndexProfController.java
+++ ProjectCode/src/professor/IndexProfController.java
@@ -1,23 +1,70 @@
 package professor;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import models.ToolKit;
 
 public class IndexProfController extends Application {
-
+	@FXML
+	private Pane ChangingPane;
+	
 	@Override
 	public void start(Stage secondaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("IndexProf.fxml"));
 			Scene scene = new Scene(root,640, 480);
-			secondaryStage.setTitle("Student Site");
+			secondaryStage.setTitle("Professor Site");
 			secondaryStage.setScene(scene);
+			secondaryStage.setMaximized(true);
 			secondaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void setChangingPane(String PanelNuevo) {
+		// cambiar panel pasando el nombre de la venta a la que se quiere ir
+		try {
+			ChangingPane.getChildren().clear();
+			ChangingPane.getChildren().add((Node) FXMLLoader.load(getClass().getResource(PanelNuevo)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void ChangingPaneProfArea(){
+		setChangingPane("DataPane.fxml");
+	}
+	
+	public void ChangingPaneSetGrade(){
+		setChangingPane("DataPane.fxml");
+	}
+	
+	public void TKClose() {
+		ToolKit.TKClose();
+	}
+	
+	public void TKSession() {
+		ToolKit.TKSession();
+	}
+	
+	public void TKHelp() {
+		ToolKit.TKHelp();
+	}
+	
+	public void TKSpecificHelp(){
+		ToolKit.TKSpecificHelp("");
+	}
+	
+	public void TKAboutUs(){
+		ToolKit.TKAboutUs();
 	}
 }
