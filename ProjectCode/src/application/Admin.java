c4b2002c747e81261ef904c64d1b072c69e427ae
pabloaaf <palvaf02@estudiantes.unileon.es>
2019-12-01 13:26:23 -0600

    new Admin view

---
+++ ProjectCode/src/application/Admin.java
@@ -0,0 +1,16 @@
+package application;
+
+import models.AcademicMember;
+
+public class Admin extends AcademicMember {
+
+	public Admin(String fName, String lName, String us, String eM) {
+		super(fName, lName, us, eM);
+	}
+
+	@Override
+	public void getMemberInfo() {
+		
+	}
+
+}
